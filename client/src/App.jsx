6ca24d0a7754d5e4612808f40dc894fd0dea547d
Hemanthyt <hemanth25052004@gmail.com>
2024-12-21 19:14:54 +0530

    routes added for dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,16 +6,28 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Applications from "./pages/Applications";
 import ApplyJob from "./pages/ApplyJob";
+import RecuriterLogin from "./components/RecuriterLogin";
+import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
+import "quill/dist/quill.snow.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { showRecruiterLogin } = useContext(AppContext);
   return (
     <div>
+      {showRecruiterLogin && <RecuriterLogin />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/applications" element={<Applications />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   );
