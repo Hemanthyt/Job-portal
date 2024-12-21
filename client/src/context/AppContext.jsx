27ee5262311c12e2b00466aa0c74089f5449dace
Hemanthyt <hemanth25052004@gmail.com>
2024-12-21 19:16:44 +0530

    added recuter login show usestate

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,6 +11,8 @@ export const AppContextProvider = (props) => {
   const [isSearched, setIsSearched] = useState(false);
   const [jobs, setJobs] = useState([]);
 
+  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
+
   //  Function to Fetch jobs
   const fetchJobs = async () => {
     setJobs(jobsData);
@@ -26,6 +28,8 @@ export const AppContextProvider = (props) => {
     setIsSearched,
     jobs,
     setJobs,
+    showRecruiterLogin,
+    setShowRecruiterLogin,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
