6598622c9a2d0c7f51d1c7662eee02eb1f9edce4
Hemanthyt <hemanth25052004@gmail.com>
2024-12-23 23:53:02 +0530

    added backend functionality with url completly

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,15 @@
 import { createContext, useEffect, useState } from "react";
-import { jobsData } from "../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
   const [searchFilter, setSearchFilter] = useState({
     title: "",
     location: "",
@@ -12,16 +18,108 @@ export const AppContextProvider = (props) => {
   const [jobs, setJobs] = useState([]);
 
   const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
+  const [companyToken, setCompanyToken] = useState(null);
+  const [companyData, setCompanyData] = useState(null);
+  const [userData, setUserData] = useState(false);
+  const [userApplications, setUserApplications] = useState([]);
 
   //  Function to Fetch jobs
   const fetchJobs = async () => {
-    setJobs(jobsData);
+    try {
+      console.log(user);
+
+      const { data } = await axios.get(backendUrl + "/api/jobs/");
+      if (data.success) {
+        setJobs(data.jobs);
+        console.log(data.jobs);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
+
+  // Function to fetch UserData
+  const fetchUserData = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backendUrl + "/api/users/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (data.success) {
+        setUserData(data.user);
+        console.log(data.user);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to fetch company Data
+  const fetchCompanyData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/company/company", {
+        headers: {
+          token: companyToken,
+        },
+      });
+      if (data.success) {
+        setCompanyData(data.company);
+        // console.log(data.company);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  // Function ro fetch user applied applications data
+  const fetchUserApplications = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backendUrl + "/api/users/applications", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (data.success) {
+        setUserApplications(data.applications);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     fetchJobs();
+    const storeCompanyToken = localStorage.getItem("companyToken");
+    if (storeCompanyToken) {
+      setCompanyToken(storeCompanyToken);
+    }
   }, []);
 
+  useEffect(() => {
+    if (user) {
+      fetchUserData();
+      fetchUserApplications();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (companyToken) {
+      fetchCompanyData();
+    }
+  }, [companyToken]);
+
   const value = {
+    backendUrl,
     searchFilter,
     setSearchFilter,
     isSearched,
@@ -30,6 +128,16 @@ export const AppContextProvider = (props) => {
     setJobs,
     showRecruiterLogin,
     setShowRecruiterLogin,
+    companyToken,
+    setCompanyToken,
+    companyData,
+    setCompanyData,
+    userData,
+    setUserData,
+    userApplications,
+    setUserApplications,
+    fetchUserData,
+    fetchUserApplications,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
