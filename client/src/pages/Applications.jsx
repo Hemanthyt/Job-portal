298792b407a944d51021bf9f6a789feb40b5be07
Hemanthyt <hemanth25052004@gmail.com>
2024-12-23 23:53:47 +0530

    added backend completly forapplications which applied by user and resume

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,23 +1,70 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { assets, jobsApplied } from "../assets/assets";
 import moment from "moment";
+import { AppContext } from "../context/AppContext";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Applications = () => {
+  const { user } = useUser();
+  const { getToken } = useAuth();
   const [isEdit, setIsEdit] = useState(false);
   const [resume, setResume] = useState(null);
+  const {
+    backendUrl,
+    userData,
+    userApplications,
+    fetchUserData,
+    fetchUserApplications,
+  } = useContext(AppContext);
+
+  const updataResume = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("resume", resume);
+
+      const token = await getToken();
+
+      const { data } = await axios.post(
+        backendUrl + "/api/users/update-resume",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        await fetchUserData();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setIsEdit(false);
+    setResume(null);
+  };
+  useEffect(() => {
+    fetchUserData();
+    fetchUserApplications();
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className=" container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
         <h2 className=" text-xl font-semibold">Your Resume</h2>
         <div className="flex gap-2 mb-6 mt-3">
-          {isEdit ? (
+          {isEdit || (userData && userData.resume == "") ? (
             <>
               <label className="flex items-center" htmlFor="resumeUpload">
                 <p className=" bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2">
-                  Select Resume
+                  {resume ? resume.name : "Select Resume"}
                 </p>
                 <input
                   id="resumeUpload"
@@ -29,7 +76,7 @@ const Applications = () => {
                 <img src={assets.profile_upload_icon} alt="" />
               </label>
               <button
-                onClick={() => setIsEdit(false)}
+                onClick={() => updataResume()}
                 className=" bg-green-100  px-4 py-2 rounded-lg border border-green-400"
               >
                 Save
@@ -38,7 +85,8 @@ const Applications = () => {
           ) : (
             <div className="flex gap-2">
               <a
-                href=""
+                href={userData ? userData.resume : ""}
+                target="_blank"
                 className=" bg-blue-50 border border-blue-500 px-4 py-2 rounded-lg"
               >
                 Resume
@@ -68,16 +116,16 @@ const Applications = () => {
             </tr>
           </thead>
           <tbody>
-            {jobsApplied.map((job, index) =>
+            {userApplications.map((job, index) =>
               true ? (
                 <tr key={index}>
                   <td className="py-3 px-4 flex items-center gap-2 border-b">
-                    <img className="w-8 h-8" src={job.logo} alt="" />
-                    {job.company}
+                    <img className="w-8 h-8" src={job.companyId.image} alt="" />
+                    {job.companyId.name}
                   </td>
-                  <td className=" py-2 px-4 border-b">{job.title}</td>
+                  <td className=" py-2 px-4 border-b">{job.jobId.title}</td>
                   <td className=" py-2 px-4 border-b max-sm:hidden">
-                    {job.location}
+                    {job.jobId.location}
                   </td>
                   <td className=" py-2 px-4 border-b max-sm:hidden">
                     {moment(job.date).format("ll")}
